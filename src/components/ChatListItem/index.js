9736be6e98689d417d363016d13c4b2ccf0a12fe
Chris Rodriguez <rodcee13@gmail.com>
2022-10-19 12:44:00 -0400

    UI-3 Props

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,54 +1,59 @@
 import { Text, View, Image, StyleSheet } from "react-native";
 import React from "react";
 
-const ChatListItem = () => {
+const ChatListItem = ({ chat }) => {
   return (
     <View style={styles.container}>
       <Image
         source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg'}}
+          uri: chat.user.image,
+        }}
         style={styles.image}
       />
       <View style={styles.content}>
         <View style={styles.row}>
-          <Text style={styles.name} numberOfLines={1}>Lukas</Text>
-          <Text style={styles.subTitle}>8:30</Text>
+          <Text style={styles.name} numberOfLines={1}>
+            {chat.user.name}
+          </Text>
+          <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
         </View>
-        <Text numberOfLines={2} style={styles.subTitle}>Hello There This is a test for the lines.  I am going to add "numberOfLines={2}" for max two lines and then I will add a "more" button</Text>
+        <Text numberOfLines={2} style={styles.subTitle}>
+          {chat.lastMessage.text}
+        </Text>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        marginHorizontal: 10,
-        marginVertical: 5,
-        height: 70,
-    },
-    image: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        marginRight: 10,
-    },
-    content: {
-        flex: 1,
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        borderBottomColor: 'lightgray',
-    },
-    row: {
-        flexDirection: 'row',
-        marginBottom: 5,
-    },
-    name: {
-        flex: 1,
-        fontWeight: 'bold'
-    },
-    subTitle: {
-        color: 'gray',
-    }
+  container: {
+    flexDirection: "row",
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  content: {
+    flex: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "lightgray",
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  name: {
+    flex: 1,
+    fontWeight: "bold",
+  },
+  subTitle: {
+    color: "gray",
+  },
 });
 
 export default ChatListItem;
