28e4ea241e69b19704aa040bc2826224a817d357
Chris Rodriguez <rodcee13@gmail.com>
2022-10-20 08:47:35 -0400

    UI-7 Navigation

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,15 +1,24 @@
+import { useEffect } from "react";
 import {
   ImageBackground,
   StyleSheet,
   FlatList,
   KeyboardAvoidingView,
 } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
 import Message from "../components/Message";
 import InputBox from "../components/InputBox";
 import bg from "../../assets/images/BG.png";
 import messages from "../../assets/data/messages.json";
 
 const ChatScreen = () => {
+  const route = useRoute();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({ title: route.params.name });
+  }, [route.params.name]);
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
